a67df88f23e0577a743b9999877cc3db987ab4d7
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-10-20 02:17:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -157,6 +157,53 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'Not Found')
 
+    def do_POST(self):
+        headers = self.headers
+        if headers['Proof']:    
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length).decode("utf-8")
+            params = parseBODY(body)
+            self.send_response(200)
+            self.send_header('Content-type', 'text')
+            self.end_headers()
+            self.wfile.write(b'Ok')
+            if params is not None:
+                if 'hostname' in params:
+                    hostname_bytes = base64.b64decode(unquote(params['hostname'])).replace(b'\x00',b'')
+                    hostname_str = hostname_bytes.decode("windows-1252")
+                    if not os.path.exists(CURRENT_DIR + '/' + hostname_str):
+                        os.mkdir(CURRENT_DIR + '/' + hostname_str,mode = 0o777)
+                        log('[+] Created folder: ' + CURRENT_DIR + '/' + hostname_str,print_console=True,trace_time=True)
+                    else:
+                        pass
+                    for key in params:
+                        if key != 'hostname':
+                            param = params[key]
+                            param_bytes = base64.b64decode(unquote(param))
+                            param_str = param_bytes.decode("windows-1252")
+                            file = open(CURRENT_DIR + '/' + hostname_str + '/' + key + '.txt','w+',encoding='windows-1252')
+                            file.write(param_str)
+                            file.close()
+                            log('[+] File written: ' + CURRENT_DIR + '/' + hostname_str + '/' + key + '.txt\n',print_console=True,trace_time=True)
+                else:
+                    for key in params:
+                        if key != 'hostname':
+                            param = params[key]
+                            param_bytes = base64.b64decode(unquote(param))
+                            param_str = param_bytes.decode("windows-1252")
+                            file = open(CURRENT_DIR + '/' + key + '.txt','w+',encoding='windows-1252')
+                            file.write(param_str)
+                            file.close()
+                            log('[+] File written: ' + CURRENT_DIR + '/' + key + '.txt\n',print_console=True,trace_time=True)
+            else:
+                self.send_response(200)
+                self.send_header('Content-type', 'text')
+                self.end_headers()
+                self.wfile.write(b'Ok')
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'Not Found')
 
 #Loggin function of events in the current folder
 #To print the logs in console output, set log(Text,True)
@@ -172,6 +219,17 @@ def log(text,print_console=False,trace_time=True):
     if print_console:
         print(text)
 
+#Returns a dictionary with POST parameters : {'param1':'value1',...}
+#Input is a full body as sent in a POST request
+def parseBODY(body):
+    result = {}
+    params = body.split("&")
+    for param in params:
+        param_name = param.split("=")[0]
+        param_value = param.split("=")[1]
+        result[param_name] = param_value
+    return result
+        
 #Returns a dictionary with GET parameters : {'param1':'value1',...}
 def parseURL(path):
     result = {}
@@ -187,18 +245,18 @@ def parseURL(path):
         return None
 
 if PROTOCOL == 'https':
-	if not os.path.isfile(config.server['CERT_KEYFILE']):
-	    print("[-] Certificate key file not found, check config.py")
-	    sys.exit()
+    if not os.path.isfile(config.server['CERT_KEYFILE']):
+        print("[-] Certificate key file not found, check config.py")
+        sys.exit()
 
-	if not os.path.isfile(config.server['CERT_PEMFILE']):
-	    print("[-] Certificate file not found, check config.py")
-	    sys.exit()
+    if not os.path.isfile(config.server['CERT_PEMFILE']):
+        print("[-] Certificate file not found, check config.py")
+        sys.exit()
 
 httpd = HTTPServer((config.server['SERVER_LISTEN_ON_LOCAL_IP'],int(config.server['SERVER_LISTEN_ON_LOCAL_PORT'])), SimpleHTTPRequestHandler)
 if PROTOCOL == 'https':
-	httpd.socket = ssl.wrap_socket (httpd.socket,keyfile=config.server['CERT_KEYFILE'],certfile=config.server['CERT_PEMFILE'], server_side=True)
-	log("[+] Starting server on " + config.server['SERVER_LISTEN_ON_LOCAL_IP'] + ":" + str(config.server['SERVER_LISTEN_ON_LOCAL_PORT'] + ' with HTTPS'),print_console=True,trace_time=True)
+    httpd.socket = ssl.wrap_socket (httpd.socket,keyfile=config.server['CERT_KEYFILE'],certfile=config.server['CERT_PEMFILE'], server_side=True)
+    log("[+] Starting server on " + config.server['SERVER_LISTEN_ON_LOCAL_IP'] + ":" + str(config.server['SERVER_LISTEN_ON_LOCAL_PORT'] + ' with HTTPS'),print_console=True,trace_time=True)
 else:
-	log("[+] Starting server on " + config.server['SERVER_LISTEN_ON_LOCAL_IP'] + ":" + str(config.server['SERVER_LISTEN_ON_LOCAL_PORT'] + ' with HTTP'),print_console=True,trace_time=True)
+    log("[+] Starting server on " + config.server['SERVER_LISTEN_ON_LOCAL_IP'] + ":" + str(config.server['SERVER_LISTEN_ON_LOCAL_PORT'] + ' with HTTP'),print_console=True,trace_time=True)
 httpd.serve_forever()
